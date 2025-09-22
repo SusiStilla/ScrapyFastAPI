53f2f719c67df55b5990f138a261196287881722
SusiStilla <suxanna96@gmail.com>
2025-09-22 17:14:51 +0200

    Refactor API: Enhance todos functionality with update and delete endpoints; remove unused code

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,4 +1,5 @@
-from typing import Union
+from typing import List, Optional
+from enum import IntEnum
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -12,28 +13,26 @@ all_todos = [
     {"id": 5, "task": "Exercise", "task_description": "Go for a run"},
 ]
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/todos/{todo_id}")
 def get_todo(todo_id: int):
     for todo in all_todos:
         if todo['id'] == todo_id:
             return {'result': todo}
-        
+
+
 @app.get("/todos")
 def get_todos(first_n: int = None):
     if first_n:
         return all_todos[:first_n]
     else:
         return all_todos
-    
+
+
 @app.post("/todos")
 def create_todo(todo: dict):
     new_todo_id = max(todos['id'] for todos in all_todos) + 1
@@ -45,6 +44,23 @@ def create_todo(todo: dict):
     }
 
     all_todos.append(new_todo)
-
     return new_todo
 
+
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id: int, updated_todo: dict):
+    for todo in all_todos:
+        if todo['id'] == todo_id:
+            todo["task"] = updated_todo["task"]
+            todo["task_description"] = updated_todo["task_description"]
+            return todo
+    return "Error, not found"
+
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(all_todos):
+        if todo["id"] == todo_id:
+            deleted_todo = all_todos.pop(index)
+            return deleted_todo
+    return "Error, not found"
